4e2ede648bd0ba166c43d96901d8074d8193d344
Azharr110 <abdazhar786@gmail.com>
2024-01-23 23:04:40 +0530

    Add card

--- src/data.js
+++ src/data.js
@@ -20,8 +20,22 @@ export default [
         location: "Online",
         openSpots: 0,
     },
+   
     {
         id: 2,
+        title: "Group Mountain Biking",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: imgBike,
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "Norway",
+        openSpots: 3,
+    },
+    {
+        id: 3,
         title: "Learn Wedding Photography",
         description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
         price: 125,
@@ -34,16 +48,17 @@ export default [
         openSpots: 27,
     },
     {
-        id: 3,
-        title: "Group Mountain Biking",
-        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
-        price: 50,
-        coverImg: imgBike,
+        id: 4,
+        title: "Learn Swimming with Katie Zaferes",
+        description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+        price: 130,
+        coverImg: imgSwim,
         stats: {
-            rating: 4.8,
-            reviewCount: 2
+            rating: 5.5,
+            reviewCount: 6
         },
-        location: "Norway",
-        openSpots: 3,
-    }
+        location: "Online",
+        openSpots: 27,
+    },
+
 ]
